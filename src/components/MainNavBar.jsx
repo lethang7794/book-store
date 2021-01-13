f7a6b785a697ee99f3f2d784174ef75822a66150
Thang Le Quang <lethang7794@gmail.com>
2021-01-13 15:35:23 +0700

    Add React Router's Link to Navbar
    - Add hamburger button

--- src/components/MainNavBar.jsx
+++ src/components/MainNavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import coderSchoolLogo from '../images/CoderSchool-Logo.svg';
 import githubMark from '../images/GitHub-Mark-64px.png';
 
@@ -8,12 +9,28 @@ const MainNavBar = () => (
     <Navbar.Brand>
       <img src={coderSchoolLogo} alt='CoderSchool' width='200px' />
     </Navbar.Brand>
-    <Nav className='mr-auto'></Nav>
-    <Nav>
-      <a href='https://github.com/lethang7794' target='_blank' rel='noreferrer'>
-        <img src={githubMark} alt='Github' width='32px' />
-      </a>
-    </Nav>
+
+    <Navbar.Toggle aria-controls='basic-navbar-nav' />
+
+    <Navbar.Collapse id='basic-navbar-nav'>
+      <Nav className='mr-auto'>
+        <Nav.Link as={Link} to='/'>
+          Home
+        </Nav.Link>
+        <Nav.Link as={Link} to='/reading'>
+          Reading List
+        </Nav.Link>
+      </Nav>
+      <Nav>
+        <Nav.Link
+          href='https://github.com/lethang7794'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <img src={githubMark} alt='Github' width='32px' />
+        </Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
   </Navbar>
 );
 
