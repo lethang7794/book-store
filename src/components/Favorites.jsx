57c015ff585df42b36d653571a84005e58887a72
Thang Le Quang <lethang7794@gmail.com>
2021-01-14 17:15:01 +0700

    Add Favotites pages

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,5 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Books from './Books';
 
-const Favorites = () => <h1>Favorites Books</h1>;
+const Favorites = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    async function fetchFavorite() {
+      setIsLoading(true);
+
+      try {
+        const BACKEND_API = process.env.REACT_APP_BACKEND_API;
+        const url = `${BACKEND_API}/favorites`;
+
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if (response.ok) {
+          setFavorites(data);
+        } else {
+          alert(
+            `Sorry. Something went wrong while we were searching for your favorite books. (${response.message})`
+          );
+        }
+      } catch (error) {
+        alert(`Sorry. We can't connect to the server.`);
+      }
+
+      setIsLoading(false);
+    }
+    fetchFavorite();
+  }, []);
+
+  return (
+    <>
+      <h1 className='text-center'>Favorites</h1>
+      {isLoading ? <div>Loading</div> : <Books books={favorites} />}
+    </>
+  );
+};
 
 export default Favorites;
